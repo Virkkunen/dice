a69d66cf92d0bb0ae02a41bab0ca0fdedb1e2fe2
Fred Schauß <vrkknn@vrkknn.net>
2023-01-20 11:12:06 -0300

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { FaCode, FaGithub } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <div className='footer'>
-      asdasd
+      <a
+        href='https://github.com/Virkkunen'
+        target="_blank"
+        rel='noopener noreferrer'
+      >
+        <FaGithub size="16px"/>
+        @Virkkunen
+      </a>
+      <a
+        href='https://github.com/Virkkunen/dice'
+        target="_blank"
+        rel='noopener noreferrer'
+      >
+        <FaCode size="16px"/>
+        source
+      </a>
     </div>
   )
 }
