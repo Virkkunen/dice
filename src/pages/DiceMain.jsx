06c4bb682d62e288cc5acbdced4503073293a092
Fred Schauß <vrkknn@vrkknn.net>
2023-01-20 10:49:38 -0300

    Footer component

--- src/pages/DiceMain.jsx
+++ src/pages/DiceMain.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import DiceGroup from '../components/DiceGroup'
 import Display from '../components/Display'
+import Footer from '../components/Footer'
 import Input from '../components/Input'
 
 export default function DiceMain() {
@@ -9,6 +10,7 @@ export default function DiceMain() {
       <Display />
       <Input />
       <DiceGroup />
+      <Footer />
     </div>
   )
 }
